6d10c403f05bde3b1249b0c42a7c6c5941ba46cb
chrisrhayden <chrisrhayden@gmail.com>
2020-09-18 01:31:16 -0700

    adding tiles

--- src/game_map.ts
+++ src/game_map.ts
@@ -1,9 +1,22 @@
-class GameMap {
+/** GameTile corresponds to the placement in the sprite map */
+export enum GameTile {
+  Nothing = 0,
+  GrassOne = 5,
+  GrassTwo = 6,
+  GrassThree = 7,
+  WallOne = 826,
+}
+
+export class GameMap {
   width: number;
   height: number;
+
+  tiles: GameTile[];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+
+    this.tiles = Array(this.width * this.height).fill(GameTile.Nothing);
   }
 }
