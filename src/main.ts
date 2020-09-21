428ad7917185d2a4924180c860ce494e39be3938
chrisrhayden <chrisrhayden@gmail.com>
2020-09-21 14:53:17 -0700

    adding entity and component logic to game
    this adds rendering for entities and a component store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as PIXI from "pixi.js";
 import {GameMap} from "./game_map/game_map";
 import {BasicMap} from "./level_gen/basic_map";
+import {Scenes} from "./scenes";
+import {Entities} from "./entities";
 
 interface GameOpts {
   sprite_sheet: string;
@@ -20,7 +22,7 @@ function init_pixi(options: AppOpts): PIXI.Application {
   const app = new PIXI.Application(options);
 
   // add the new app to the current page
-  document.body.appendChild(app.view);
+  document.getElementById("game-div").appendChild(app.view);
 
   return app;
 }
@@ -32,8 +34,6 @@ function load_assets(sprite_sheet: string): Promise<PIXI.Spritesheet> {
   return new Promise<PIXI.Spritesheet>((resolve, reject) => {
     loader.add(sprite_sheet).load((_loader, resources) => {
       const sprites: PIXI.Spritesheet = resources[sprite_sheet].spritesheet;
-
-      console.log(sprites);
 
       if (sprites && sprites["textures"]) {
         resolve(sprites);
@@ -47,16 +47,30 @@ function load_assets(sprite_sheet: string): Promise<PIXI.Spritesheet> {
 class Game {
   options: GameOpts;
 
-  // a container for the map
-  map_container: PIXI.Container;
+  // display containers, sprites are add to a container and pixi will draw them
+  // from there
+  containers: Record<string, PIXI.Container>;
 
   // the current sprite_sheet that everything will be drawn from
   sprite_sheet: PIXI.Spritesheet;
 
+  // the sprites for the map
+  sprite_map: PIXI.Sprite[];
+
+  // a cache for entity sprites
+  entity_sprites: Record<number, PIXI.Sprite>;
+
   // a struct to hold the game map
   game_map: GameMap;
-  // the sprites for the part of map that is currently in window view
-  sprite_map: PIXI.Sprite[];
+
+  // a class containing the game scenes
+  scenes: Scenes;
+
+  // a class to make unique entities
+  entities: Entities;
+
+  // the current scene being used
+  current_scene: number;
 
   // just set the options and the init_game function will load the data
   constructor(game_opts: GameOpts) {
@@ -64,24 +78,50 @@ class Game {
   }
 
   // this is the real constructor so we can load things asynchronously
-  async init_game(): Promise<PIXI.Container> {
+  async init_game(): Promise<Record<string, PIXI.Container> | boolean> {
     this.sprite_sheet = await load_assets(this.options.sprite_sheet);
 
-    // get a basic dungeon map
-    this.game_map = new BasicMap(50, 36).make_basic_map();
+    this.entities = new Entities();
 
-    // get a container for the map sprites
-    this.map_container = new PIXI.Container();
+    this.scenes = new Scenes();
+
+    this.current_scene = this.scenes.new_scene();
+
+    const cur_scene = this.scenes.get_scene(this.current_scene);
+
+    // get a basic dungeon map
+    this.game_map = new BasicMap(50, 36)
+      .make_basic_map(this.entities, cur_scene);
+
+    this.containers = {};
+
+    // get a containers for the sprites
+    this.containers["map"] = new PIXI.Container();
+    this.containers["entities"] = new PIXI.Container();
 
     // make the sprite_map array
     this.sprite_map = Array(this.game_map.tiles.length);
 
-
     // add the given sprites to the container and the sprite_map
     this.make_sprite_map();
 
-    // return the container to be added by th app
-    return this.map_container;
+    this.entity_sprites = {};
+
+    // add the active entities to the screen
+    this.render_entities();
+
+    // a sanity check
+    if (this.current_scene === 0
+      || this.containers["map"].children.length === 0
+      || this.containers["entities"].children.length === 0) {
+
+      console.error("did no make the game data correctly");
+
+      return false;
+    }
+
+    // return the containers to be added by the app
+    return this.containers;
   }
 
   // make the sprite_map from the game_map
@@ -127,7 +167,7 @@ class Game {
       this.sprite_map[i].y = y;
 
       // add the sprite to the map_container so it will get added to the app
-      this.map_container.addChild(this.sprite_map[i]);
+      this.containers["map"].addChild(this.sprite_map[i]);
 
       x += tile_w;
       row_count += 1;
@@ -136,13 +176,72 @@ class Game {
     return true;
   }
 
+  // update all active entities to there current position
+  render_entities(): boolean {
+    const cur_scene = this.scenes.get_scene(this.current_scene);
+
+    // get an iterator over the keys and values for the active entities
+    const entries = Object.entries(cur_scene.components.active_entities);
+
+    for (const [id, entity] of entries) {
+      const pos = cur_scene.components.position[id];
+
+      const x = pos % this.game_map.width;
+      const y = Math.floor(pos / this.game_map.width);
+
+      if (id in this.entity_sprites) {
+        // multiply by the sprite size to place on screen correctly
+        this.entity_sprites[id].x = x * this.options.sprite_size[0];
+        this.entity_sprites[id].y = y * this.options.sprite_size[0];
+
+      } else {
+        this.entity_sprites[id] = new PIXI.Sprite(
+          this.sprite_sheet["textures"][entity.tile.toString()]
+        );
+
+        this.entity_sprites[id].x = x * this.options.sprite_size[0];
+        this.entity_sprites[id].y = y * this.options.sprite_size[0];
+
+        this.containers["entities"].addChild(this.entity_sprites[id]);
+      }
+    }
+
+    // now for the player
+    const pos = cur_scene.components.position[cur_scene.player];
+
+    const x = pos % this.game_map.width;
+    const y = Math.floor(pos / this.game_map.width);
+
+    if (cur_scene.player in this.entity_sprites) {
+      this.entity_sprites[cur_scene.player].x = x * this.options.sprite_size[0];
+      this.entity_sprites[cur_scene.player].y = y * this.options.sprite_size[0];
+    } else {
+      this.entity_sprites[cur_scene.player] = new PIXI.Sprite(
+        this.sprite_sheet["textures"]["27"]
+      );
+
+      this.entity_sprites[cur_scene.player].x = x * this.options.sprite_size[0];
+      this.entity_sprites[cur_scene.player].y = y * this.options.sprite_size[0];
+
+      this.containers["entities"].addChild(this.entity_sprites[cur_scene.player]);
+    }
+
+    return true;
+  }
+
   // TODO: make game
   run_game(): boolean {
+    if (!this.render_entities()) {
+      console.error("could not render entities");
+
+      return false;
+    }
+
     return true;
   }
 }
 
-async function run(): Promise<void> {
+async function main(): Promise<void> {
   // pixi options
   const app_opts: AppOpts = {
     width: 800,
@@ -152,10 +251,8 @@ async function run(): Promise<void> {
   // get a new pixi.js context
   const app = init_pixi(app_opts);
 
-  const sprite_sheet = "assets/pngs/colored_packed.json";
-
   const opts: GameOpts = {
-    sprite_sheet,
+    sprite_sheet: "assets/pngs/colored_packed.json",
     sprite_size: [16, 16],
   };
 
@@ -165,16 +262,24 @@ async function run(): Promise<void> {
   // wait for the game to load the assets
   const container = await game.init_game();
 
+  if (!container || typeof container === "boolean") {
+    return;
+  }
+
   // add the game container to the app
-  app.stage.addChild(container);
+  app.stage.addChild(container["map"]);
+  app.stage.addChild(container["entities"]);
 
   // add the game logic to the app loop
-  app.ticker.add(() => game.run_game());
-}
+  app.ticker.add(() => {
+    const ok = game.run_game();
 
-function main() {
-  // so we can evenly catch errors and do something about them
-  run().catch(console.log);
+    if (!ok) {
+      // the place where the error happened should try and print the error
+      // hmm, idk how i feel about this
+      app.ticker.stop();
+    }
+  });
 }
 
 main();
