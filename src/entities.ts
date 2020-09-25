1f70b01b3088e2c616d5c656d17fbd4e307c0ed4
chrisrhayden <chrisrhayden@gmail.com>
2020-09-24 21:47:32 -0700

    im not really sure if this is better

--- src/entities.ts
+++ src/entities.ts
@@ -1,15 +1,21 @@
+/** a basic entity manager
+ *
+ * there really isn't anything to this as its just a way to be sure that
+ * entities are unique
+ *
+ * a zero id will never be used so it can be used as a sort of null value if
+ * ever needed
+ */
 export class Entities {
   id: number;
 
   constructor() {
-    this.id = 1;
+    this.id = 0;
   }
 
   new_id(): number {
-    const old_id = this.id;
-
     this.id += 1;
 
-    return old_id;
+    return this.id;
   }
 }
