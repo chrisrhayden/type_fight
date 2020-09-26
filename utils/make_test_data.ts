14cee3bd3d198050b81498bb3a1ddbad188f4759
chrisrhayden <chrisrhayden@gmail.com>
2020-09-26 01:52:04 -0700

    adding a thing to make test data

---
+++ utils/make_test_data.ts
@@ -0,0 +1,31 @@
+/** this will make test data like levels */
+
+// import {GameMap} from "../src/game_map/game_map";
+import * as fs from "fs";
+import {BasicMap} from "../src/level_gen/basic_map";
+import {FeatureGenerator} from "../src/feature_generator";
+import {Entities} from "../src/entities";
+import {Scene} from "../src/scenes";
+
+function main(): void {
+  const seed = 3333;
+  const feature = new FeatureGenerator(seed);
+  const width = 50;
+  const height = 36;
+
+  const entitys = new Entities();
+
+  const scene = new Scene();
+
+  const basic_map = new BasicMap(feature, width, height);
+
+  const made_map = basic_map.make_map(entitys, scene);
+
+
+  fs.writeFileSync("./test/test_data_basic_map.json",
+    JSON.stringify(made_map));
+
+  return;
+}
+
+main();
