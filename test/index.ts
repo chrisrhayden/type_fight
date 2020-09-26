966c23e9b1a09ee3dfa5fa585a2de65b8e540cf7
chrisrhayden <chrisrhayden@gmail.com>
2020-09-26 01:52:38 -0700

    adding test context to easily import from here

---
+++ test/index.ts
@@ -0,0 +1,7 @@
+export {GameMap} from "../src/game_map/game_map";
+export {BasicMap} from "../src/level_gen/basic_map";
+export {FeatureGenerator} from "../src/feature_generator";
+export {Entities} from "../src/entities";
+export {Scene} from "../src/scenes";
+export {Rect} from "../src/game_map/map_utils";
+export {GameTile} from "../src/tiles";
