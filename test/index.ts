6542e7c4bc99230d8a2b2ac2e400f9f42f269602
chrisrhayden <chrisrhayden@gmail.com>
2020-09-26 16:44:11 -0700

    add Scene to export

--- test/index.ts
+++ test/index.ts
@@ -3,6 +3,6 @@ export {GameMap} from "../src/game_map/game_map";
 export {BasicMap} from "../src/level_gen/basic_map";
 export {FeatureGenerator} from "../src/feature_generator";
 export {Entities} from "../src/entities";
-export {Scene} from "../src/scenes";
+export {Scenes, Scene} from "../src/scenes";
 export {Rect} from "../src/game_map/map_utils";
 export {GameTile} from "../src/tiles";
