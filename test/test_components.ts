84f698ef98e1653134fad6379b2a9bcab09fd16e
chrisrhayden <chrisrhayden@gmail.com>
2020-09-26 02:51:59 -0700

    add more structure; add comment about how useless this test is

--- test/test_components.ts
+++ test/test_components.ts
@@ -6,14 +6,18 @@ import {Components} from "./index";
 describe("test Components", () => {
   const components = new Components();
 
-  it("makes a blank objext in all values", () => {
-    const entries = Object.values(components);
+  describe("gets created correctly", () => {
+    // TODO: this is kinda useless as javascript doesn't have class level
+    // variable definitions i think
+    it("makes a blank objext in all values", () => {
+      const entries = Object.values(components);
 
-    for (const value of entries) {
-      assert.ok(typeof value === "object", "type of value is not an objext");
+      for (const value of entries) {
+        assert.ok(typeof value === "object", "type of value is not an objext");
 
-      assert.ok(Object.keys(value).length === 0,
-        "value is something other then an empty object");
-    }
+        assert.ok(Object.keys(value).length === 0,
+          "value is something other then an empty object");
+      }
+    });
   });
 });
