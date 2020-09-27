fa0b2b8070f5b6b51c9f25b0140afa3a0203cb31
chrisrhayden <chrisrhayden@gmail.com>
2020-09-26 17:47:58 -0700

    adding re-exports for other tests

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,15 @@
-export {Components} from "../src/components";
-export {GameMap} from "../src/game_map/game_map";
+export {Entities} from "../src/entities";
+export {Components, BaseEntity} from "../src/components";
+export {GameMap, TerrainData} from "../src/game_map/game_map";
 export {BasicMap} from "../src/level_gen/basic_map";
 export {FeatureGenerator} from "../src/feature_generator";
-export {Entities} from "../src/entities";
 export {Scenes, Scene} from "../src/scenes";
 export {Rect} from "../src/game_map/map_utils";
 export {GameTile} from "../src/tiles";
+export {
+  move_by,
+  move_up,
+  move_down,
+  move_left,
+  move_right
+} from "../src/systems/movement";
