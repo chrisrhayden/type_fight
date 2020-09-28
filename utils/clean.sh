0be820b1a25d54ba96c345b2e48df838ba13150f
chrisrhayden <chrisrhayden@gmail.com>
2020-09-28 14:19:21 -0700

    update to handle empty dist directory
    though with webpack we dont need to rely on compiling first

--- utils/clean.sh
+++ utils/clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-if [[ -d dist ]]; then
+if [[ -n $(ls -aA ./dist/) ]]; then
     rm -r ./dist/*
 fi
+
+for ff in $(find ./public -name "*bundle*.js"); do
+    if [[ -f "$ff" ]]; then
+        rm "$ff"
+    fi
+done
