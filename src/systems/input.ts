e4a02b4bf4194759d05d80b9d04f4301a0b3236c
chrisrhayden <chrisrhayden@gmail.com>
2020-09-28 18:13:25 -0700

    update to use move_or_attack

--- src/systems/input.ts
+++ src/systems/input.ts
@@ -1,23 +1,59 @@
 import {Scene} from "../scenes";
-import {
-  move_up,
-  move_down,
-  move_left,
-  move_right
-} from "./movement";
+import {move_or_attack} from "./player";
 
 export function handle_input(scene: Scene, evt: KeyboardEvent): boolean {
   if (evt.type === "keydown") {
-    // case fall though as the default is so fucking stupid
+    let new_pos = -1;
+
     switch (evt.key) {
+      // up
+      case "ArrowUp":
       case "w":
-        return move_up(scene, scene.player);
+        new_pos =
+          scene.components.position[scene.player] - scene.game_map.width;
+        break;
+      // left
+      case "ArrowLeft":
       case "a":
-        return move_left(scene, scene.player);
+        new_pos = scene.components.position[scene.player] - 1;
+        break;
+      case "ArrowDown":
       case "s":
-        return move_down(scene, scene.player);
+        new_pos =
+          scene.components.position[scene.player] + scene.game_map.width;
+        break;
+      // right
+      case "ArrowRight":
       case "d":
-        return move_right(scene, scene.player);
+        new_pos = scene.components.position[scene.player] + 1;
+        break;
+      // up-right
+      case "e":
+        new_pos =
+          (scene.components.position[scene.player] - scene.game_map.width) + 1;
+        break;
+      // up-left
+      case "q":
+        new_pos =
+          (scene.components.position[scene.player] - scene.game_map.width) - 1;
+        break;
+      // down-right
+      case "x":
+        new_pos =
+          (scene.components.position[scene.player] + scene.game_map.width) + 1;
+        break;
+      // down-left
+      case "z":
+        new_pos =
+          (scene.components.position[scene.player] + scene.game_map.width) - 1;
+        break;
+    }
+
+
+    if (new_pos !== -1) {
+      return move_or_attack(scene, new_pos);
     }
   }
+
+  return false;
 }
