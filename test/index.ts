f8360b91adcac8205970cbde7a70bcd2faf9e824
chrisrhayden <chrisrhayden@gmail.com>
2020-09-28 18:06:56 -0700

    remove unneeded functions
    all entities can just use the same move_to function

--- test/index.ts
+++ test/index.ts
@@ -6,10 +6,3 @@ export {FeatureGenerator} from "../src/feature_generator";
 export {Scenes, Scene} from "../src/scenes";
 export {Rect} from "../src/game_map/map_utils";
 export {GameTile} from "../src/tiles";
-export {
-  move_by,
-  move_up,
-  move_down,
-  move_left,
-  move_right
-} from "../src/systems/movement";
